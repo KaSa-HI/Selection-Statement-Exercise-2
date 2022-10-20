82847aa25fa82777549231f2beda3a27061613df
koikiri <kahvisabatino@gmail.com>
2022-10-19 19:46:01 -1000

    Exercise2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Console.Write("What is your favourite school subject? ");
+            string favSchoolSubject = Console.ReadLine(); 
+
+            switch (favSchoolSubject)
+            {
+                case "Math":
+                    Console.WriteLine($"{favSchoolSubject}");
+                    Console.WriteLine("Ew.");
+                    break;
+
+                case "English":
+                    Console.WriteLine($"{favSchoolSubject}");
+                    Console.WriteLine("Lets eat grandma or Lets eat, grandma");
+                    break;
+
+                case "Science":
+                    Console.WriteLine($"{favSchoolSubject}");
+                    Console.WriteLine("WEIRD SCIENCE!!");
+                    break;
+
+                case "P.E":
+                    Console.WriteLine($"{favSchoolSubject}");
+                    Console.WriteLine("Health is important");
+                    break;
+
+                case "Study Hall":
+                    Console.WriteLine($"{favSchoolSubject}");
+                    Console.WriteLine("Snooze Break, nice.");
+                    break;
+
+                 default:
+                    Console.WriteLine($"{favSchoolSubject}");
+                    Console.WriteLine("Oh, you're playing hookie.");
+                    break;
+            }
         }
     }
 }
